b4cee3e9e4fe53be42113aae31a987449116a428
Mes0903 <mes900903@gmail.com>
2023-12-28 21:29:27 +0800

    split Fuse implementation out.

--- src/main.cpp
+++ src/main.cpp
@@ -10,239 +10,15 @@
 
 #define FUSE_USE_VERSION 30
 
+#include "Fuse.h"
+
 #include <fuse.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <time.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
 
-// ... //
-
-char dir_list[256][256];
-int curr_dir_idx = -1;
-
-char files_list[256][256];
-int curr_file_idx = -1;
-
-char files_content[256][256];
-int curr_file_content_idx = -1;
-
-void add_dir(const char *dir_name)
-{
-  curr_dir_idx++;
-  strcpy(dir_list[curr_dir_idx], dir_name);
-}
-
-int is_dir(const char *path)
-{
-  path++;    // Eliminating "/" in the path
-
-  for (int curr_idx = 0; curr_idx <= curr_dir_idx; curr_idx++)
-    if (strcmp(path, dir_list[curr_idx]) == 0)
-      return 1;
-
-  return 0;
-}
-
-void add_file(const char *filename)
-{
-  curr_file_idx++;
-  strcpy(files_list[curr_file_idx], filename);
-
-  curr_file_content_idx++;
-  strcpy(files_content[curr_file_content_idx], "");
-}
-
-int is_file(const char *path)
-{
-  path++;    // Eliminating "/" in the path
-
-  for (int curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++)
-    if (strcmp(path, files_list[curr_idx]) == 0)
-      return 1;
-
-  return 0;
-}
-
-int get_file_index(const char *path)
-{
-  path++;    // Eliminating "/" in the path
-
-  for (int curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++)
-    if (strcmp(path, files_list[curr_idx]) == 0)
-      return curr_idx;
-
-  return -1;
-}
-
-void write_to_file(const char *path, const char *new_content)
-{
-  int file_idx = get_file_index(path);
-
-  if (file_idx == -1)    // No such file
-    return;
-
-  strcpy(files_content[file_idx], new_content);
-}
-
-// ... //
-
-static int do_getattr(const char *path, struct stat *st)
-{
-  st->st_uid = getuid();    // The owner of the file/directory is the user who mounted the filesystem
-  st->st_gid = getgid();    // The group of the file/directory is the same as the group of the user who mounted the filesystem
-  st->st_atime = time(NULL);    // The last "a"ccess of the file/directory is right now
-  st->st_mtime = time(NULL);    // The last "m"odification of the file/directory is right now
-
-  if (strcmp(path, "/") == 0 || is_dir(path) == 1) {
-    st->st_mode = S_IFDIR | 0755;
-    st->st_nlink = 2;    // Why "two" hardlinks instead of "one"? The answer is here: http://unix.stackexchange.com/a/101536
-  }
-  else if (is_file(path) == 1) {
-    st->st_mode = S_IFREG | 0644;
-    st->st_nlink = 1;
-    st->st_size = 1024;
-  }
-  else {
-    return -ENOENT;
-  }
-
-  return 0;
-}
-
-static int do_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
-{
-  filler(buffer, ".", NULL, 0);    // Current Directory
-  filler(buffer, "..", NULL, 0);    // Parent Directory
-
-  if (strcmp(path, "/") == 0)    // If the user is trying to show the files/directories of the root directory show the following
-  {
-    for (int curr_idx = 0; curr_idx <= curr_dir_idx; curr_idx++)
-      filler(buffer, dir_list[curr_idx], NULL, 0);
-
-    for (int curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++)
-      filler(buffer, files_list[curr_idx], NULL, 0);
-  }
-
-  return 0;
-}
-
-static int do_read(const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi)
-{
-  int file_idx = get_file_index(path);
-
-  if (file_idx == -1)
-    return -1;
-
-  char *content = files_content[file_idx];
-
-  memcpy(buffer, content + offset, size);
-
-  return strlen(content) - offset;
-}
-
-static int do_mkdir(const char *path, mode_t mode)
-{
-  path++;
-  add_dir(path);
-
-  return 0;
-}
-
-static int do_mknod(const char *path, mode_t mode, dev_t rdev)
-{
-  path++;
-  add_file(path);
-
-  return 0;
-}
-
-static int do_write(const char *path, const char *buffer, size_t size, off_t offset, struct fuse_file_info *info)
-{
-  write_to_file(path, buffer);
-
-  return size;
-}
-
-static int do_create(const char *path, mode_t mode, struct fuse_file_info *info)
-{
-  if (get_file_index(path) != -1)
-    return -1;
-  path++;
-  add_file(path);
-  return 0;
-}
-
-// 給 touch 用的
-static int do_utimens(const char *, const struct timespec tv[2])
-{
-  return 0;
-}
-
-// 刪除 dir
-static int do_rmdir(const char *path)
-{
-  // 如果找不到 path ， 不做任何事
-  path++;
-  int touch_target = 0;
-  int target_dir_idx = curr_dir_idx;
-
-  for (int curr_idx = 0; curr_idx <= curr_dir_idx; curr_idx++) {
-    if (strcmp(path, dir_list[curr_idx]) == 0) {
-      touch_target = 1;
-      target_dir_idx = curr_idx;
-      break;
-    }
-  }
-  // 從存放 path 的那一格開始將所有 dir 的 name 往前移一格，如果 path 不存在不會進入這個 loop
-  for (int curr_idx = target_dir_idx; curr_idx < curr_dir_idx; curr_idx++) {
-    strcpy(dir_list[curr_idx], dir_list[curr_idx + 1]);
-  }
-
-  // 如果找到 path，刪除 dir，所以 curr_dir_idx 減少 1 ;
-  curr_dir_idx -= touch_target;
-
-  return 0;
-}
-
-// 刪除 file
-static int do_unlink(const char *path)
-{
-  // 如果找不到 path ， 不做任何事
-  path++;
-  int touch_target = 0;
-  int target_file_idx = curr_file_idx;
-  for (int curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++) {
-    printf("searching\n");
-    if (strcmp(path, files_list[curr_idx]) == 0) {
-      touch_target = 1;
-      target_file_idx = curr_idx;
-      printf("found target\n");
-      break;
-    }
-  }
-
-
-  // 從存放 path 的那一格開始將所有 file 的 name 和 content 往前移一格，如果 path 不存在不會進入這個 loop
-  for (int curr_idx = target_file_idx; curr_idx < curr_file_idx; curr_idx++) {
-    printf("test\n");
-    strcpy(files_list[curr_idx], files_list[curr_idx + 1]);
-    strcpy(files_content[curr_idx], files_content[curr_idx + 1]);
-  }
-
-  // 如果找到 path，刪除 path，所以 curr_dir_idx 減少 1 ;
-  curr_file_idx -= touch_target;
-
-  return 0;
-}
+#include <iostream>
 
 static struct fuse_operations operations;
 
-int main(int argc, char *argv[])
+static void set_operations()
 {
   operations.getattr = do_getattr;
   operations.readdir = do_readdir;
@@ -254,6 +30,10 @@ int main(int argc, char *argv[])
   operations.utimens = do_utimens;
   operations.rmdir = do_rmdir;
   operations.unlink = do_unlink;
+}
 
+int main(int argc, char *argv[])
+{
+  set_operations();
   return fuse_main(argc, argv, &operations, NULL);
 }
